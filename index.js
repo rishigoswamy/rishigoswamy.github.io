488667b17c1ca496235aacefe021a6963868553a
rishigoswamy <goswamyrishi@gmail.com>
2024-06-20 17:33:56 -0400

    Added Intro

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
         } else {
             x.style.display = "none";
         }
-    }, 2200);   
+    }, 2200);
     new Typed('#console2', {
         strings: ['^2320 echo education<br>'],
         typeSpeed: 100,
@@ -27,22 +27,24 @@
             x.style.display = "none";
         }
     }, 4800);
+
     new Typed('#console3', {
-        strings: ['^5000 ls<br>'],
-        typeSpeed: 100,
-        showCursor: false
-    });
-    setTimeout(() => {
-        var x = document.getElementsByClassName("s3")[0];
-        console.log(x);
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none";
-        }
-    }, 6000);
+            strings: ['^4000 echo introduction<br>'],
+            typeSpeed: 100,
+            showCursor: false
+        });
+        setTimeout(() => {
+            var x = document.getElementsByClassName("s3")[0];
+            console.log(x);
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }, 6500);
+
     new Typed('#console4', {
-        strings: ['^6600 cat intro.txt<br>'],
+        strings: ['^7200 ls<br>'],
         typeSpeed: 100,
         showCursor: false
     });
@@ -54,5 +56,21 @@
         } else {
             x.style.display = "none";
         }
-    }, 8500);
+    }, 7500);
+
+
+    new Typed('#console5', {
+        strings: ['^8000 cat intro.txt<br>'],
+        typeSpeed: 100,
+        showCursor: false
+    });
+    setTimeout(() => {
+        var x = document.getElementsByClassName("s5")[0];
+        console.log(x);
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }, 10000);
 })();
